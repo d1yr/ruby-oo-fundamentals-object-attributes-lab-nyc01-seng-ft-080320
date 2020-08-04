6503cfe30a43155196ef29578a183a3ae8b63aa7
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-08-04 17:00:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,17 @@
 class Dog
-   def initialize()
-     @name = name
-   end
-   def name
-     @name
-   end
-    def name=(new_name)
-     @name = new_name
-   end
- end
-
+  
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  
+  def name
+    @name 
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end 
+end
